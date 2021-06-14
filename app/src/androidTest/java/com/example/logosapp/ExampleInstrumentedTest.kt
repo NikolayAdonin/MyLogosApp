49afc279f9145fcbd8d3a12d6777594a860c42c4
NikolayAdonin <gegennumann@yandex.ru>
2021-06-14 09:32:15 +0300

    First add

--- app/src/androidTest/java/com/example/logosapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/logosapp/ExampleInstrumentedTest.kt
@@ -19,7 +19,6 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-
     @get:Rule
     public var activityActivityTestRule = ActivityTestRule(MainActivity::class.java)
 
