338292d130e3b95bced3b860880bc9d909129ea2
NikolayAdonin <gegennumann@yandex.ru>
2021-12-15 13:45:36 +0300

    new version

---
+++ src/main/kotlin/com/example/exampler/LogosApp/LogosAppApplication.kt
@@ -0,0 +1,11 @@
+package com.example.exampler.LogosApp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LogosAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<LogosAppApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/exampler/LogosApp/LogosAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.exampler.LogosApp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LogosAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
